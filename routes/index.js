763413541376205c1d04d5666ab4507240287892
Aaron Burdick <burdick.aaron@gmail.com>
2016-11-26 01:14:33 +0900

    dustin came to my house

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'aburd\'s Portfolio | aburdのポートフォリオ' });
 });
 
+router.get('/test', function(req, res, next) {
+	res.render('index', { title: 'A Test', layout: 'layout-test' })
+})
+
 module.exports = router;
