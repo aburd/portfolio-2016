5ea59d9dfb6071db1c649c004864951015a7d0e4
Aaron Burdick <burdick.aaron@gmail.com>
2017-04-26 22:34:42 +0900

    Add two more works

--- models/insert-works.js
+++ models/insert-works.js
@@ -12,17 +12,18 @@ db.on('error', console.error.bind(console, 'connection error:'))
 
 db.once('open', function(){
 	// get info from json
-	var worksPath = path.join(__dirname, "..", "public", "images", "images.json")
+	var worksPath = path.join(__dirname, "..", "data", "projects.json")
 	var worksData = JSON.parse(fs.readFileSync(worksPath))
 
-	worksData.forEach(( work ) => {
+	worksData.forEach(function ( work ) {
 		var theWork = new Work(work)
-		theWork.save((err, theWork) => {
+		theWork.save(function (err, theWork) {
 			if(err) return console.error("err: " +  err)
 			console.log(theWork.sayMyName())
+
+      db.close()
 		})
 
 	})
 
 })
-
